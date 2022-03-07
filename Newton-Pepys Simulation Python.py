67ea39cf7f29e6a8ecce630e40eadd290849f520
Angelfeather0 <77983941+Angelfeather0@users.noreply.github.com>
2022-03-07 14:04:39 +0100

    Improved version
    User no longer has to input what proposition they want to test. The program runs all three.

--- Newton-Pepys Simulation Python.py
+++ Newton-Pepys Simulation Python.py
@@ -1,27 +1,26 @@
 import random 
 
-option = input("What proposition would you like to simulate (A, B, C)? \n")
 period = int(input("For how many throws? \n"))
 
-if option.upper() == 'A':
-    optionNumber = 1
-elif option.upper() == 'B':
-    optionNumber = 2
-elif option.upper() == 'C':
-    optionNumber = 3
+optionNumber = 1
+string = ""
+for proposition in range(0, 3):
+    sixesToss = 0
 
-sixesToss = 0
-for i in range(1, period+1):
-    diceToss = []
-    for j in range (0, (optionNumber * 6)):
-        diceToss.append(random.randint(1, 6))
+    for throw in range(1, period+1):
+        diceToss = []
 
-    if diceToss.count(6) >= optionNumber:
-        sixesToss += 1
+        for dice in range (0, (optionNumber * 6)):
+            diceToss.append(random.randint(1, 6))
+
+        if diceToss.count(6) >= optionNumber:
+            sixesToss += 1
+
+    string += "In " + str(period) + " throws, " + str(sixesToss) + " throws had, at least, " + str(optionNumber) + " sixes in them. The probability is " + str(sixesToss/period) + "\n"
+
+    optionNumber += 1
 
 openFile = open("Newton-Pepys Results File.txt", 'a')
-
-string = option.upper() + ": In " + str(period) + " throws, " + str(sixesToss) + " throws had, at least, " + str(optionNumber) + " sixes in them. The probability is " + str(sixesToss/period) + "\n"
 
 openFile.write(string)
 
